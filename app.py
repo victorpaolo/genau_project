b280dff44b79c820c02b4671e25cbc753810b139
victorpaolo <51170664+victorpaolo@users.noreply.github.com>
2024-01-23 01:03:21 +0000

    Added home and Bootstrap

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return "Hello world"
+    return render_template('home.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
