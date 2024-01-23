4cda2cdd4df97126a707ea51d61d185ce7a8024f
victorpaolo <51170664+victorpaolo@users.noreply.github.com>
2024-01-23 00:54:52 +0000

    Added Flask

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return "Hello world"
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
