af2a1d96cf543c41801b166e05ee3f658b52cb45
victorpaolo <51170664+victorpaolo@users.noreply.github.com>
2024-01-28 02:06:23 +0000

    Refactor in app.py and database.py

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
-from database import engine
-from sqlalchemy import text
+from database import load_vokabular_from_db
+
 
 app = Flask(__name__)
 
-def load_vokabular_from_db():
-  with engine.connect() as conn:
-    result = conn.execute(text("select * from genauproject.vokabular"))
-  def row_to_dict(row):
-    return {column: getattr(row, column) for column in row.keys()}
-  worts = []
-  for row in result.all():
-    worts.append(row_to_dict(row))
-  return worts
 
 @app.route('/')
 def hello():
--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import sqlalchemy
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import os
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']  
@@ -11,3 +11,14 @@ engine = create_engine(
       "ssl_ca": "/etc/ssl/cert.pem"
     }
   })
+
+
+def load_vokabular_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from genauproject.vokabular"))
+    def row_to_dict(row):
+      return {column: getattr(row, column) for column in row.keys()}
+    worts = []
+    for row in result.all():
+      worts.append(row_to_dict(row))
+    return worts
